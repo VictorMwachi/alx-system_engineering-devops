0c30678bb1dfa5635af1e2f3876d9b2af31c909c
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-02-08 16:31:41 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Uses the JSON placeholder api to query data about an employee
+"""
+Uses the JSON placeholder api to query data about an employee
 """
 
 from requests import get
 from sys import argv
 
 if __name__ == '__main__':
-    site_url = 'https://jsonplaceholder.typicode.com/'
-    to_do_url = site_url + f"user/{argv[1]}/todos"
-    name_url = site_url + f"users/{argv[1]}"
-    emp_dict = get(name_url).json()
-    to_do_dict = get(to_do_url).json()
-    name = emp_dict.get('name')
-    total_tasks = len(to_do_dict)
-    tasks_done = sum([i['completed'] for i in to_do_dict])
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
+
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
     print("Employee {} is done with tasks({}/{}):"
-          .formart(name, tasks_done, total_tasks))
-    for task in to_do_dict:
-        if task.get('completed'):
-            print('\t {}'.formart(task['title']))
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
