0411822dd6d54382c2b65a5fccb9f96c1732b452
Victor Mwachi <mudakizevictor@gmail.com>
2023-02-08 05:12:37 -0800

    gather

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from requests import get
+from sys import argv
+if __name__ == '__main__':
+    site_url = 'https://jsonplaceholder.typicode.com/'
+    to_do_url = site_url + f"user/{argv[1]}/todos"
+    name_url = site_url + f"users/{argv[1]}"
+    emp_dict = get(name_url).json()
+    to_do_dict = get(to_do_url).json()
+    name = emp_dict.get('name')
+    total_tasks = len(to_do_dict)
+    tasks_done = sum([i['completed'] for i in to_do_dict])
+    print("Employee {} is done with tasks({}/{}):".formart(name, tasks_done, total_tasks))
+    for task in to_do_dict:
+        if task.get('completed'):
+            print('\t {}'.formart(task['title']))
