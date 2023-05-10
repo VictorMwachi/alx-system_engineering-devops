2c5b9f42c37bb6ef3b8c5fd12b137d0ef2290f3c
Victor Mwachi <mudakizevictor@gmail.com>
2023-05-10 00:25:53 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,12 @@ contains a function that queries the Reddit API and returns the number
 of subscribers (not active users, total subscribers) for a given subreddit
 """
 
-import request
+import requests
 
 def number_of_subscribers(subreddit):
     """returns number of subcribers"""
-    if type(subreddit) is str:
-        r = request.get("https://www.reddit.com/dev/api/r/{}/about.json".formart(subreddit),
-        headers={'User-Agent': '0x16-api_advanced:project:0-subs)'}).json()
+    if subreddit is not None and type(subreddit) is str:
+        url = "https://www.reddit.com/dev/api/r/{}/about.json".format(subreddit)
+        r = requests.get(url,headers={'User-Agent': '0x16-api_advanced:project:0-subs)'}).json()
         subs = r.get("data", {}).get("subscribers", 0)
         return subs
