60c9dbe0d25a61b9080079e65eda084291c81383
Victor Mwachi <mudakizevictor@gmail.com>
2023-05-10 01:20:18 -0700

    0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,12 @@ of subscribers (not active users, total subscribers) for a given subreddit
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns number of subcribers"""
     if subreddit is not None and type(subreddit) is str:
-        url = "https://www.reddit.com/dev/api/r/{}/about.json".format(subreddit)
-        r = requests.get(url,headers={'User-Agent': '0x16-api_advanced:project:0-subs)'}).json()
+        ur = "https://www.reddit.com/dev/api/r/{}/about.json".format(subreddit)
+        headers = {'User-Agent': '0x16-api_advanced:project:0-subs)'}
+        r = requests.get(url, headers=headers).json()
         subs = r.get("data", {}).get("subscribers", 0)
         return subs
